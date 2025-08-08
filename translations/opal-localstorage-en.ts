2da0722c35a79b6afaa9c9058636f829ade3b13b
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2025-08-08 20:53:32 +0200

    Update translations

--- translations/opal-localstorage-en.ts
+++ translations/opal-localstorage-en.ts
@@ -4,48 +4,48 @@
 <context>
     <name>Opal.LocalStorage</name>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="94"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"/>
         <source>Database upgrade failed</source>
         <translation>Database upgrade failed</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="95"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"/>
         <source>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</source>
         <translation>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="101"/>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="126"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"/>
         <source>Developer information:</source>
         <translation>Developer information:</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="110"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"/>
         <source>Invalid database version</source>
         <translation>Invalid database version</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="111"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"/>
         <source>The app cannot start because the database has version %1 but only version %2 is supported.</source>
         <translation>The app cannot start because the database has version %1 but only version %2 is supported.</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="118"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"/>
         <source>Database Maintenance</source>
         <translation>Database Maintenance</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="119"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"/>
         <source>Please be patient and allow up to 30 seconds for this.</source>
         <translation>Please be patient and allow up to 30 seconds for this.</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="122"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"/>
         <source>Database issue</source>
         <translation>Database issue</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="123"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"/>
         <source>An unexpected issue occurred in the database. Try restarting the app.</source>
         <translation>An unexpected issue occurred in the database. Try restarting the app.</translation>
     </message>
