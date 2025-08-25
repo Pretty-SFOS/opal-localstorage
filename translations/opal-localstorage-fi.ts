5c73239489049a5413904ac89719985fa61b1e07
Elmeri Länsiharju <notgonnatellyou@protonmail.com>
2025-08-25 21:01:59 +0200

    Translated using Weblate (Finnish)
    Currently translated at 66.6% (6 of 9 strings)
    
    Co-authored-by: Elmeri Länsiharju <notgonnatellyou@protonmail.com>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-localstorage/fi/
    Translation: Opal for SailfishOS/Module: Opal.LocalStorage

--- translations/opal-localstorage-fi.ts
+++ translations/opal-localstorage-fi.ts
@@ -4,50 +4,50 @@
 <context>
     <name>Opal.LocalStorage</name>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"></location>
         <source>Database upgrade failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Tietokannan päivitys epäonnistui</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"></location>
         <source>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tietokannan päivittämisessä versiosta %1 versioon %2 tapahtui virhe. Ilmoitathan tästä ongelmasta.</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"/>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"></location>
         <source>Developer information:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"></location>
         <source>Invalid database version</source>
-        <translation type="unfinished"></translation>
+        <translation>Virheellinen tietokantaversio</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"></location>
         <source>The app cannot start because the database has version %1 but only version %2 is supported.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"></location>
         <source>Database Maintenance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"></location>
         <source>Please be patient and allow up to 30 seconds for this.</source>
-        <translation type="unfinished"></translation>
+        <translation>Olethan kärsivällinen, sillä tässä saattaa kestää jopa 30 sekuntia.</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"></location>
         <source>Database issue</source>
-        <translation type="unfinished"></translation>
+        <translation>Tietokantavirhe</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"></location>
         <source>An unexpected issue occurred in the database. Try restarting the app.</source>
-        <translation type="unfinished"></translation>
+        <translation>Tietokannassa ilmaantui odottamaton virhe. Yritä käynnistää sovellus uudelleen.</translation>
     </message>
 </context>
 </TS>
