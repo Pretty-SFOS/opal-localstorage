937a3c56660c47790e850d33e8cee037d689b103
Alex Zander <zanderhelpsout@proton.me>
2025-11-28 06:01:05 +0100

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (9 of 9 strings)
    
    Co-authored-by: Alex Zander <zanderhelpsout@proton.me>
    Translate-URL: https://hosted.weblate.org/projects/opal/opal-localstorage/ro/
    Translation: Opal for SailfishOS/Module: Opal.LocalStorage

--- translations/opal-localstorage-ro.ts
+++ translations/opal-localstorage-ro.ts
@@ -6,48 +6,48 @@
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"/>
         <source>Database upgrade failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Actualizarea bazei de date a eșuat</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"/>
         <source>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</source>
-        <translation type="unfinished"></translation>
+        <translation>A apărut o eroare la actualizarea bazei de date de la versiunea %1 la versiunea %2. Vă rugăm să raportați această problemă.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"/>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"/>
         <source>Developer information:</source>
-        <translation type="unfinished"></translation>
+        <translation>Informații pentru dezvoltatori:</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"/>
         <source>Invalid database version</source>
-        <translation type="unfinished"></translation>
+        <translation>Versiune nevalidă a bazei de date</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"/>
         <source>The app cannot start because the database has version %1 but only version %2 is supported.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aplicația nu poate porni, deoarece baza de date are versiunea %1, dar este acceptată numai versiunea %2.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"/>
         <source>Database Maintenance</source>
-        <translation type="unfinished"></translation>
+        <translation>Întreținerea bazei de date</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"/>
         <source>Please be patient and allow up to 30 seconds for this.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vă rugăm să aveți răbdare și să acordați până la 30 de secunde pentru acest lucru.</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"/>
         <source>Database issue</source>
-        <translation type="unfinished"></translation>
+        <translation>Problema bazei de date</translation>
     </message>
     <message>
         <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"/>
         <source>An unexpected issue occurred in the database. Try restarting the app.</source>
-        <translation type="unfinished"></translation>
+        <translation>A apărut o problemă neașteptată în baza de date. Încercați să reporniți aplicația.</translation>
     </message>
 </context>
 </TS>
