bece4b3af81479b065bb839ab800211a3c317b27
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2025-12-22 12:52:26 +0100

    Update translations

--- translations/opal-localstorage-fi.ts
+++ translations/opal-localstorage-fi.ts
@@ -4,48 +4,48 @@
 <context>
     <name>Opal.LocalStorage</name>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="206"/>
         <source>Database upgrade failed</source>
         <translation>Tietokannan päivitys epäonnistui</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="207"/>
         <source>An error occurred while upgrading the database from version %1 to version %2. Please report this issue.</source>
         <translation>Tietokannan päivittämisessä versiosta %1 versioon %2 tapahtui virhe. Ilmoitathan tästä ongelmasta.</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"></location>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="213"/>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="238"/>
         <source>Developer information:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="222"/>
         <source>Invalid database version</source>
         <translation>Virheellinen tietokantaversio</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="223"/>
         <source>The app cannot start because the database has version %1 but only version %2 is supported.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="230"/>
         <source>Database Maintenance</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="231"/>
         <source>Please be patient and allow up to 30 seconds for this.</source>
         <translation>Olethan kärsivällinen, sillä tässä saattaa kestää jopa 30 sekuntia.</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="234"/>
         <source>Database issue</source>
         <translation>Tietokantavirhe</translation>
     </message>
     <message>
-        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"></location>
+        <location filename="../Opal/LocalStorage/MessageHandler.qml" line="235"/>
         <source>An unexpected issue occurred in the database. Try restarting the app.</source>
         <translation>Tietokannassa ilmaantui odottamaton virhe. Yritä käynnistää sovellus uudelleen.</translation>
     </message>
